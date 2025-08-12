342528d59acf238a7ba83e0e7f5afc785da9fa04
yoondohyeon73-collab <yoondohyeon73@gmail.com>
2025-08-12 17:45:04 +0900

    Add files via upload
    tff파일 파서 만듬
    저녁밥 먹음 기분이 좋음

--- SoftcupcakeEnterpriseDataBase/main.go
+++ SoftcupcakeEnterpriseDataBase/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"sedb/modules/parsers"
+)
+
+func main() {
+
+	tff := "Title : \"test_table\"\nTABLE_S BEGIN \n COL1 number,COL2 text\nEND DATA_SECTION :\n Data->"
+
+	var tokens []parsers.Tff_token
+
+	err := parsers.ParseHeader(tff, &tokens)
+	if err == 1 {
+		print("ERROR!")
+	}
+
+	for i, tok := range tokens {
+		fmt.Printf("Token %d: Type=%v, Value=%v\n", i, tok.Token_type, tok.Token)
+	}
+
+}
